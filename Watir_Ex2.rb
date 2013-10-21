fd3751ef1af20f05479f255c10bfa066a2b5e996
radhika khatamakka <rkhatamakka@chathamfinancial.com>
2013-10-20 20:43:43 -0400

    Example 2 from Chzy
    Introducing Modules

---
+++ Watir_Ex2.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'watir-webdriver'
+require_relative 'Watir_Ex1'
+
+include AdoptionHelper
+
+goto_the_puppy_adoption_site
+adopt_puppy_number 1
+continue_adopting_puppies
+adopt_puppy_number 2
+checkout_with('Rad', 'Blah', 'Blah@Blah.Com', 'Check')
+verify_page_contains 'Thank you for adopting a puppy!'
+close_the_browser
