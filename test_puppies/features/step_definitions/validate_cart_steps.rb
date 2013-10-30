be6a3f063a337878a63443c8e658770afed4aa8b
radhika khatamakka <rkhatamakka@chathamfinancial.com>
2013-10-29 21:32:45 -0400

    Introducing the page object

--- test_puppies/features/step_definitions/validate_cart_steps.rb
+++ test_puppies/features/step_definitions/validate_cart_steps.rb
@@ -1,17 +1,27 @@
 
-Then(/^I should see "([^"]*)" as the name for line item 1$/) do |name1|
- fail unless @browser.table(:index =>0)[0][1].text.should include name1
+Then(/^I should see "([^"]*)" as the name for line item (\d+)$/) do |name, line_item|
+ fail unless cart_line_item(line_item.to_i)[1].text.should include name
 end
-When(/^I should see "([^"]*)" as the subtotal for line item 1$/) do |subtotal1|
-  fail unless @browser.table(:index =>0)[0][3].text.should == subtotal1
+When(/^I should see "([^"]*)" as the subtotal for line item (\d+)$/) do |subtotal, line_item|
+  fail unless cart_line_item(line_item.to_i)[3].text.should == subtotal
 end
 When(/^I should see "([^"]*)" as the cart total$/) do |total|
   fail unless @browser.td(:class => 'total_cell').text.should == total
 end
+=begin
 
 When(/^I should see "([^"]*)" as the name for line item 2$/) do |name2|
   fail unless @browser.table(:index =>0)[6][1].text.should include name2
 end
 When(/^I should see "([^"]*)" as the subtotal for line item 2$/) do |subtotal2|
   fail unless @browser.table(:index=>0)[6][3].text.should == subtotal2
+end
+=end
+
+def row_for(line_item)
+   (line_item-1)*6
+end
+
+def cart_line_item(line_item)
+ @browser.table(:index =>0)[(row_for(line_item))]
 end
--- test_puppies/features/support/pages/shopping_cart_page.rb
+++ test_puppies/features/support/pages/shopping_cart_page.rb
@@ -2,4 +2,50 @@ class ShoppingCartPage
   def initialize(browser)
     @browser = browser
   end
+
+  def name_for_line_item(line_item)
+    cart_line_item(line_item)[1]
+  end
+
+  def subtotal_for_line(line_item)
+    cart_line_item(line_item)[3]
+  end
+
+  def cart_total
+    @browser.td(:class => 'total_cell').text
+  end
+
+  def proceed_to_check_out
+    @browser.button(:value=> 'Complete the Adoption').click
+  end
+
+  def continue_shopping
+    @browser.button(:value => 'Adopt Another Puppy').click
+  end
+
+  def name=(name)
+    @browser.text_field(:id=> 'order_name').set(name)
+  end
+
+  def address=(address)
+    @browser.text_field(:id=> 'order_address').set(address)
+  end
+
+  def email=(email)
+    @browser.text_field(:id=> 'order_email').set(email)
+  end
+
+  def pay_type=(pay_type)
+    @browser.select_list(:id=> 'order_pay_type').select(pay_type)
+  end
+
+  private
+  def row_for(line_item)
+    (line_item-1)*6
+  end
+
+  def cart_line_item(line_item)
+    @browser.table(:index =>0)[(row_for(line_item))]
+  end
+
 end
